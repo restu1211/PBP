11cda07fa909c0fe1df2df5e1157e8515f507944
Restu Ramdani_TI22A <114630690+restu1211@users.noreply.github.com>
2023-10-04 21:36:15 +0700

    Add files via upload

---
+++ Tugas PBP SESI2-RestuRamdani.js
@@ -0,0 +1,12 @@
+console.log(`------Tugas Sesi 2-------`);
+console.log(`Program Berbasis Platform`);
+console.log(`-------------------------`);
+let firstName = "Restu"
+let lastName = "Ramdani"
+let age = 19;//
+let isMarried = age == 25
+
+console.log("Nama Depan        : " + firstName);
+console.log("Nama Belakang     : " + lastName);
+console.log("umur              : " + age )
+console.log(`Status Pernikahan :` + isMarried);
