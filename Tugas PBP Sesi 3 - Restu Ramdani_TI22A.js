3d359c9509d04b5f1d32a68f1e4bbd80f0ded0f1
Restu Ramdani_TI22A <114630690+restu1211@users.noreply.github.com>
2023-10-20 19:46:43 +0700

    Add files via upload

---
+++ Tugas PBP Sesi 3 - Restu Ramdani_TI22A.js
@@ -0,0 +1,28 @@
+console.log("Nama       : Restu Ramdani")
+console.log("Nim        : 20220040138")
+console.log("Kelas      : TI22A")
+console.log("Mata Kuliah: Pemrograman Berbasis Platform")
+console.log("==================================================")
+
+// Variabel dengan tipe object
+let restaurant = {
+    name: "Resto A",
+    city: "Sukabumi",
+    favoriteDrink: " sweet iced tea",
+    favoriteFood: "Bakso",
+    isVegan: false
+};
+
+// Variabel dengan tipe array
+let restaurants = [
+    {
+        name: "Resto AlimRugi",
+        city: "Sukabumi",
+        favoriteDrink: "Sweet Iced Tea",
+        favoriteFood: "Bakso",
+        isVegan: false
+    }
+];
+
+console.log(restaurant);
+console.log(restaurants)
